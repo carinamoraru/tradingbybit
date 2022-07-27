299ec37fc0fb263916b80c797695698f57eee1a4
carina <carinamoraru0520@gmail.com>
2022-07-28 01:58:15 +0800

    'fd'

--- automated_bybit.py
+++ automated_bybit.py
@@ -411,6 +411,7 @@ def dashboard():
 def test():
     if request.method == 'POST':
         logging.error('%s action', request.data)
+        data = json.loads(request.data)
         data = request.data
         logging.error('%s action', data['message'])
         # action = data['action']
